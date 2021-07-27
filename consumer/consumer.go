d3de656553fbf4871b799127c65e6dc6e0bd74a4
sank-project <sangra.nitikarachma@gmail.com>
2021-07-27 16:17:51 +0700

    complete consumer code

---
+++ consumer/consumer.go
@@ -0,0 +1,64 @@
+package consumer
+
+import (
+	"os"
+
+	"github.com/Shopify/sarama"
+	"github.com/sirupsen/logrus"
+)
+
+//kafka consumer hold sarama consumer
+type KafkaConsumer struct {
+	Consumer sarama.Consumer
+}
+
+//Consume function to consume message from kafka
+
+func (c *KafkaConsumer) Consume(topics []string, signals chan os.Signal) {
+	chanMessage := make(chan *sarama.ConsumerMessage, 256)
+
+	for _, topic := range topics {
+		partitionList, err := c.Consumer.Partitions(topic)
+		if err != nil {
+			logrus.Errorf("Unable to get partition got err %v", err)
+			continue
+		}
+
+		for _, partition := range partitionList {
+			go consumeMessage(c.Consumer, topic, partition, chanMessage)
+		}
+
+	}
+	logrus.Infof("Kafka is consuming...")
+
+ConsumerLoop:
+	for {
+		select {
+		case msg := <-chanMessage:
+			logrus.Infof("New message from kafka, message %v", string(msg.Value))
+		case sig := <-signals:
+			if sig == os.Interrupt {
+				break ConsumerLoop
+			}
+		}
+	}
+
+}
+
+func consumeMessage(consumer sarama.Consumer, topic string, partition int32, c chan *sarama.ConsumerMessage) {
+	msg, err := consumer.ConsumePartition(topic, partition, sarama.OffsetNewest)
+	if err != nil {
+		logrus.Errorf("Unable to consume message partition %v got erro %v", partition, err)
+		return
+	}
+	defer func() {
+		if err := msg.Close(); err != nil {
+			logrus.Errorf("Unable to close partition %v: %v", partition, err)
+		}
+	}()
+
+	for {
+		msg := <-msg.Messages()
+		c <- msg
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module kafka-consumer
 
 go 1.16
+
+require (
+	github.com/Shopify/sarama v1.29.1
+	github.com/joho/godotenv v1.3.0
+	github.com/sirupsen/logrus v1.8.1
+)
